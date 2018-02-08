4a4f1162e605b17dac2b5d838a111e80e30ff8b3
Lex <alexreferal@gmail.com>
2018-02-08 02:01:14 +0100

    decorator

---
+++ src/main/java/patterns/Decorator.java
@@ -0,0 +1,37 @@
+package patterns;
+
+public class Decorator {
+    public static void main(String[] args) {
+        FileStream fileStream = new FileBufferefReader(new FileStreamReader());
+        fileStream.read();
+    }
+}
+interface FileStream{
+    void read();
+}
+class FileStreamReader implements FileStream{
+
+    @Override
+    public void read() {
+        System.out.println("read file");
+    }
+}
+abstract class FileDecorater implements FileStream{
+    FileStream fileStream;
+
+    FileDecorater(FileStream fileStream) {
+        this.fileStream = fileStream;
+    }
+    public abstract void read();
+}
+class FileBufferefReader extends FileDecorater{
+    public FileBufferefReader(FileStream fileStream) {
+        super(fileStream);
+    }
+
+    @Override
+    public void read() {
+        fileStream.read();
+        System.out.println("buffered read");
+    }
+}
